dabb15aac95924a1770a48f2de87079acebe46fb
jospi96 <113528331+jospi96@users.noreply.github.com>
2023-01-23 16:57:20 +0100

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,8 @@ describe('AppComponent', () => {
   it(`should have as title 'angular2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular2');
+
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular2 app is running!');
-  });
+ 
 });
