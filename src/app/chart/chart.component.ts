5466178a3278454f63d5072dafda67018b26f4a6
Guillaume H <66718486+guillaumehussong@users.noreply.github.com>
2023-01-27 16:49:51 +0100

    indented code in the Chart component

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -3,8 +3,6 @@ import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
 
 import {  cityData } from '../model';
 
-
-
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
@@ -19,24 +17,26 @@ export class ChartComponent implements OnInit{
   ngOnInit(): void {
 
     let dataTemp= this.data ? this.data : {categories:[{score_out_of_10:0, name:"", color:"#000"}
-  ],teleport_city_score:0};
+      ],teleport_city_score:0};
+
     let arrayTemp:number[]=[]
     let colorArray:string[]=[]
-for(let paramater of dataTemp.categories){
-  
-  this.label.push(paramater.name)
- 
-  arrayTemp.push(paramater.score_out_of_10)
-  colorArray.push(paramater.color)
-}
-this.chartDataset=[
-  {data:arrayTemp, label:this.label, backgroundColor:colorArray}
-]
- this.cityRating=dataTemp.teleport_city_score
- 
+
+    for(let paramater of dataTemp.categories){
+      this.label.push(paramater.name)
+      arrayTemp.push(paramater.score_out_of_10)
+      colorArray.push(paramater.color)
+    }
+
+    this.chartDataset=[
+      {data:arrayTemp, label:this.label, backgroundColor:colorArray}
+    ]
+
+    this.cityRating=dataTemp.teleport_city_score
+
 }
 
 
 }
- 
-  
+
+
